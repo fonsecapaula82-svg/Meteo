d120396becff59ce4f9682bf930b9702d174112c
fonsecapaula82-svg <fonsecapaula82@gmail.com>
2025-09-23 20:57:01 -0500

    Added weather api

--- src/script.js
+++ src/script.js
@@ -1,8 +1,25 @@
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "3a344ae76b8dt7aoa36bf37f3d5544b0";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric";
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
