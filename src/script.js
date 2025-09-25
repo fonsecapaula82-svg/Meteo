9eb2de32d546267e4695bf0a8b012d3f95e87f00
fonsecapaula82-svg <fonsecapaula82@gmail.com>
2025-09-25 10:14:50 -0500

    Removed UL forecast block

--- src/script.js
+++ src/script.js
@@ -7,6 +7,7 @@ function showTemperature(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
@@ -14,6 +15,7 @@ function showTemperature(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${Math.round(response.data.wind.speed)} Km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
